debc7317b49d70b4f725fee104e4a5b4755df98d
Gasper Kojek <gasper.kojek@gmail.com>
2016-12-03 16:10:00 +0100

    Added TimeSlot model and updated Event model with it

---
+++ app/src/main/java/com/ws1617/iosl/pubcrawl20/DataModels/TimeSlot.java
@@ -0,0 +1,44 @@
+package com.ws1617.iosl.pubcrawl20.DataModels;
+
+import java.util.Date;
+
+/**
+ * Created by Gasper Kojek on 3. 12. 2016.
+ * Github: https://github.com/ribafish/
+ */
+
+public class TimeSlot {
+    private long pubId;
+    private Date startTime;
+    private Date endTime;
+
+    public TimeSlot(long pubId, Date startTime, Date endTime) {
+        this.pubId = pubId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public long getPubId() {
+        return pubId;
+    }
+
+    public void setPubId(long pubId) {
+        this.pubId = pubId;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+}
