2aae16f91f9ea98e8ebbc7fb026a8f5715054f14
DESKTOP-4FQ225R\Haneen <haneen.zak@gmail.com>
2016-12-13 12:47:09 +0100

    QRcode

--- app/src/main/java/com/ws1617/iosl/pubcrawl20/NewEvent/ShareEventDialog.java
+++ app/src/main/java/com/ws1617/iosl/pubcrawl20/NewEvent/ShareEventDialog.java
@@ -10,12 +10,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.ws1617.iosl.pubcrawl20.R;
+
+import static android.R.attr.width;
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.WHITE;
+import static android.support.v7.appcompat.R.attr.height;
+import static java.awt.font.TextAttribute.WIDTH;
 
 /**
  * Created by Haneen on 12/7/2016.
@@ -27,6 +35,7 @@ public class ShareEventDialog extends DialogFragment {
     //Views
     ImageView mQrCodeImg;
     View rootView;
+    String mBarcodeData = "test";
 
     public ShareEventDialog() {
     }
@@ -39,37 +48,51 @@ public class ShareEventDialog extends DialogFragment {
         rootView = LayoutInflater.from(getActivity()).inflate(R.layout.view_invite_dialog
                 , null);
         alertDialog.setView(rootView);
-
+        initDialogView(rootView);
         return alertDialog.create();
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        initDialogView(rootView);
+       // initDialogView(rootView);
     }
 
     void initDialogView(View rootView) {
-     /*   mQrCodeImg = (ImageView) rootView.findViewById(R.id.invite_barcode);
-        QRCodeWriter writer = new QRCodeWriter();
-        String content = "test";
-        // this is a small sample use of the QRCodeEncoder class from zxing
+        mQrCodeImg = (ImageView) rootView.findViewById(R.id.invite_barcode);
+        Bitmap bmp = null;
         try {
-            // generate a 150x150 QR code== €Bitmap.Config.RGB_565);
-            for (int x = 0; x < width; x++) {
-                for (int y = 0; y < height; y++) {<*
-                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Colo≉
-                mQrCodeImg.setImageBitmap(bmp);
-            }
-        } catch (WriterException e) { //eek }
+
+            bmp = encodeAsBitmap(mBarcodeData);
+            mQrCodeImg.setImageBitmap(bmp);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
         }
-    }ckedListener);
+    }
 
 
-        mPubsListView = (Spinner) view.findViewById(R.id.pub_dialog_pubs_list);
-        mPubsListView.setOnItemSelectedListener(pubListOnItemSelectedListener);
-        //TODO should be fetched from the DB
-*/
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, 1800, 900, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, w, 0, 0, w, h);
+        return bitmap;
     }
 }
 
