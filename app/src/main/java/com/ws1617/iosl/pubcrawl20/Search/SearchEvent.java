7beb2c5b6ca08d3bed1daf83b69f3340df8c39c0
Haneen <haneen.abdallah@campus.tu-berlin.de>
2017-02-04 21:48:41 +0100

    search activity

---
+++ app/src/main/java/com/ws1617/iosl/pubcrawl20/Search/SearchEvent.java
@@ -0,0 +1,11 @@
+package com.ws1617.iosl.pubcrawl20.Search;
+
+import android.app.Activity;
+import android.app.Notification;
+
+/**
+ * Created by Haneen on 04/02/2017.
+ */
+
+public class SearchEvent extends Activity {
+}
