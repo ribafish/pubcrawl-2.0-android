79d4e1c780409802532417491c324565ded4c01d
ickehier29 <ickehier29@users.noreply.github.com>
2017-02-08 08:44:39 +0100

    Fixing being stupid

--- app/src/main/java/com/ws1617/iosl/pubcrawl20/signInActivity.java
+++ app/src/main/java/com/ws1617/iosl/pubcrawl20/signInActivity.java
@@ -36,13 +36,13 @@ public class SignInActivity extends AppCompatActivity implements
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_sign_in);
 
     // Views
     mStatusTextView = (TextView) findViewById(R.id.status);
 
     // Button listeners
-    findViewById(R.id.sign_in_button_2).setOnClickListener(this);
+    findViewById(R.id.sign_in_button).setOnClickListener(this);
     findViewById(R.id.sign_out_button).setOnClickListener(this);
     findViewById(R.id.disconnect_button).setOnClickListener(this);
 
@@ -184,12 +184,12 @@ public class SignInActivity extends AppCompatActivity implements
 
   private void updateUI(boolean signedIn) {
     if (signedIn) {
-      findViewById(R.id.sign_in_button_2).setVisibility(View.GONE);
+      findViewById(R.id.sign_in_button).setVisibility(View.GONE);
       findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
     } else {
       mStatusTextView.setText(R.string.signed_out);
 
-      findViewById(R.id.sign_in_button_2).setVisibility(View.VISIBLE);
+      findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
       findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
     }
   }
@@ -197,7 +197,7 @@ public class SignInActivity extends AppCompatActivity implements
   @Override
   public void onClick(View v) {
     switch (v.getId()) {
-      case R.id.sign_in_button_2:
+      case R.id.sign_in_button:
         signIn();
         break;
       case R.id.sign_out_button:
